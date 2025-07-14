3f819e99e21c8e4a2dca9a0661f6105b4ea9579f
mdashraful24 <mdashrafulislam2882@gmail.com>
2025-07-14 15:54:14 +0600

    update login page padding

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
     };
 
     return (
-        <div className={`flex items-center justify-center p-4 text-black ${theme === "dark"
+        <div className={`flex items-center justify-center px-4 pt-8 pb-16 text-black ${theme === "dark"
             ? "bg-gray-900"
             : "bg-gray-100"
                 }`}>
